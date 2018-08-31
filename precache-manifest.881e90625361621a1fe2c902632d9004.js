e967768b4424a469039d91f34509463d388ba537
puterakahfi <puterakahfi@gmail.com>
2018-08-31 19:04:46 +0700

    deploy

---
+++ precache-manifest.881e90625361621a1fe2c902632d9004.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/uiux-tools/robots.txt"
+  },
+  {
+    "revision": "2058ea4abe811947451a",
+    "url": "/uiux-tools/js/chunk-vendors.5b240b47.js"
+  },
+  {
+    "revision": "b86fb982bad46d649201",
+    "url": "/uiux-tools/js/app.37630d8b.js"
+  },
+  {
+    "revision": "083b480c704835c0eac2",
+    "url": "/uiux-tools/js/about.5d688836.js"
+  },
+  {
+    "revision": "41c86966469726eb4bcd306c4a6dbe38",
+    "url": "/uiux-tools/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/uiux-tools/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b86fb982bad46d649201",
+    "url": "/uiux-tools/css/app.303d2445.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+importScripts(
+  "/uiux-tools/precache-manifest.881e90625361621a1fe2c902632d9004.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "uiux-tools"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
